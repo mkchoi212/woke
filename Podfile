2e658b2917bff423a62e86b85c42f460900a888d
Mike JS. Choi <mkchoi212@icloud.com>
2018-10-28 13:36:33 -0500

    Initial project setup with Transition UI framework

---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+
+target 'woke' do
+  pod 'Transition'
+end
---
+++ Pods/Target Support Files/Pods-woke/Pods-woke-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_woke : NSObject
+@end
+@implementation PodsDummy_Pods_woke
+@end
